5a424889472ec106c39d1508ccc7b5cc1126b405
Mabon ap Gwyn <mabonapgwyn@hotmail.co.uk>
2021-09-08 17:53:37 +0100

    refactored and added web_helpers.rb to call methods in tests

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,12 +1,7 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Mabon'
-    fill_in :player_2_name, with: 'Sophie'
-    click_button 'Submit'
-
+    sign_in_and_play
     # save_and_open_page
-
-    expect(page).to have_content 'Mabon vs. Sophie'
+    expect(page).to have_content 'player1 vs. player2'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'See hit points' do
   scenario "player 1 is able to see player 2's hitpoints" do
-    visit('/')
-    fill_in :player_1_name, with: 'Jack'
-    fill_in :player_2_name, with: 'Mark'
-    click_button 'Submit'
-    expect(page).to have_content "Mark's hitpoints: 1500"
+    sign_in_and_play
+    expect(page).to have_content "player2's hitpoints: 1500"
   end
 end
